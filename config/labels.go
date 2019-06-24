73c9195a78cfb7427f2a7b70ffff12781b9f9044
Geoff Bourne <itzgeoff@gmail.com>
2019-06-24 09:35:37 -0500

    Use underscore delimiter in discovered labels

--- config/labels.go
+++ config/labels.go
@@ -1,19 +1,17 @@
 /*
- *    Copyright 2018 Rackspace US, Inc.
+ * Copyright 2019 Rackspace US, Inc.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package config
@@ -39,12 +37,12 @@ var (
 	SystemPrefixes = []string{DiscoveredPrefix}
 
 	ArchLabel        = DiscoveredLabel("arch")
-	BiosVendorLabel  = DiscoveredLabel("bios-vendor")
-	BiosVersionLabel = DiscoveredLabel("bios-version")
+	BiosVendorLabel  = DiscoveredLabel("bios_vendor")
+	BiosVersionLabel = DiscoveredLabel("bios_version")
 	HostnameLabel    = DiscoveredLabel("hostname")
 	OsLabel          = DiscoveredLabel("os")
 	SerialNoLabel    = DiscoveredLabel("serial")
-	XenIdLabel       = DiscoveredLabel("xen-id")
+	XenIdLabel       = DiscoveredLabel("xen_id")
 )
 
 // ComputeLabels reads any labels specified in the config file.
